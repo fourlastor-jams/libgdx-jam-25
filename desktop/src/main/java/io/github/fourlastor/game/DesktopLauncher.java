f9fa2fd410a9395c329917af78afcc0569af51c5
Daniele Conti <fourlastor@users.noreply.github.com>
2023-06-23 23:53:12 +0100

    Set correct title in game

--- desktop/src/main/java/io/github/fourlastor/game/DesktopLauncher.java
+++ desktop/src/main/java/io/github/fourlastor/game/DesktopLauncher.java
@@ -12,7 +12,7 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setForegroundFPS(60);
-        config.setTitle("Ur-derwater");
+        config.setTitle("The Royal Game of Ur");
         setWindowedMode(config);
         new Lwjgl3Application(GdxGame.createGame(true), config);
     }
